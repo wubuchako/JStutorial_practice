92377d8a7a2a9d4850bb049f22b1aa66b74d1599
wubuchako <56101474+wubuchako@users.noreply.github.com>
2019-12-13 09:19:00 +0900

    add some practice code

--- JStutorial.js
+++ JStutorial.js
@@ -662,7 +662,51 @@ function randomRange(myMin,myMax){
 var myRandom = randomRange(5, 15);
 console.log(myRandom);
 
+//
+function checkSign(num) {
+    return num > 0 ? "positive" : num < 0 ? "negative" :"zero"
+}
+console.log(checkSign(10));
+//
+function freezeObj(){
+    "use strict";
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
 
+    Object.freeze(MATH_CONSTANTS);
+
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch (ex){
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+
+const PI = freezeObj();
+//
+const magic = () => new Date();
+//
+var myConcat= (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1,2],[3,4,5]));
+//
+const realNumberArray = [4,5.6, -9.8,3.14];
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x*x);
+    return squaredIntegers;
+}
+const squareIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+//
+
+const increment = (function (){
+    return function increment(number,value = 1){
+        return number + value;
+    };
+})();
+console.log(increment(5,2)); //7
+console.log(increment(5));//6
 
 
 
